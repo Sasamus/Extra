5c51d6bfb146c61395402b608001d63301ad138d
Sasamus <labus93@gmail.com>
2015-02-14 13:33:37 +0100

    Did some tweaking to the project so threads and c++11 things wwork + copied in the version of BlockingTicketDequeue.h that have comments

--- src/BlockingTicketDeque.h
+++ src/BlockingTicketDeque.h
@@ -11,18 +11,22 @@
 
 using std::deque;
 
-
 class BlockingTicketDeque {
 public:
 	BlockingTicketDeque(int maxSize);
+
 	void pushBack(QTicket *qt);
+	/* Push qt at the end. If full, block until there is space. */
+
 	void popFrontAndService(int deskNumber);
+	/* Pop the front element off the front of theDeque and invoke the service
+	function on it. If empty, block until not empty.*/
 
 private:
 	deque<QTicket*> theDeque;
 	const int maxSz;
-
 };
+
 
 
 #endif
